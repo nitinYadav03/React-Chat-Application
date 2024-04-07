f153873066a6d8f71c011ef619a477b4b356e40b
Nitin Yadav <yadavnitin332001@gmail.com>
2024-04-07 15:42:17 +0530

    first commit

---
+++ chat-app/src/App.jsx
@@ -0,0 +1,7 @@
+import Register from './pages/Register';
+import Login from './pages/Login';
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ chat-app/src/pages/Login.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <>
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            class="mx-auto h-10 w-auto"
+            src="/images/chat-icon.gif"
+            alt="Chat-Icon"
+          />
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Register
+          </h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form class="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div class="flex items-center justify-between">
+                <label
+                  for="password"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div class="text-sm">
+                  <a
+                    href="#"
+                    class="font-semibold text-sky-600 hover:text-sky-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div class="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="current-password"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p class="mt-10 text-center text-sm text-gray-500">
+            You do have an account
+            <a
+              href="#"
+              class="font-semibold leading-6 text-sky-600 hover:text-sky-500"
+            >
+              Login
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
